155ca0702515356d0f41c85ddd665ede65909993
shaikhFaris <144804581+shaikhFaris@users.noreply.github.com>
2024-04-27 20:17:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,27 +8,26 @@ const PORT=4444
 
 // Use pre tags for code blocks or for giving proper formating
 
-// views engine setup
-app.set("views",path.join(__dirname,'views')); // importnt for serving static files like css
+// views engine setup . Used for rendering index.ejs file
+app.set("views",path.join(__dirname,'views')); // important for serving static files like css
 app.set('view engine', 'ejs'); 
 
 // app configuration
-app.use(express.static(path.join(__dirname,'public'))) // importnt for serving static files like css
+app.use(express.static(path.join(__dirname,'public'))) // important for serving static files like css
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://10.200.232.107:4444');
+    res.setHeader('Access-Control-Allow-Origin', 'Your_website_address');
     // You can also specify other CORS headers as needed
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-// connecting to database
-await mongoose.connect("mongodb://localhost:27017/EmployeeDB")
 
+// connecting to database
+await mongoose.connect("Your_mongodb_connection_string")
 
 app.get("/",async (req,res)=>{
     // res.render("index",{result}); This can be used if u directly put result in .ejs file using ejs templates 
     res.render("index")
-    
 })
 
 app.get("/api",async (req,res)=>{
@@ -39,5 +38,5 @@ app.get("/api",async (req,res)=>{
 
 
 app.listen(PORT,()=>{
-    console.log("Server running at 10.200.232.107:4444" );
+    console.log("Server running at Your_websiteAdress"+ PORT);
 })
